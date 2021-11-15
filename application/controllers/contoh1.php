c1a3a5437c6d880f3776c2c0f7f4ba085c495ce3
Vinanrmdhn <kkfinan172@gmail.com>
2021-11-15 16:11:33 +0700

    menambahkan tugas pertemuan 2-4

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Vinan Ramadhan
+ Saya tinggal di daerah Bojongkaum cipedes tasikmalaya
+ olah raga yang saya sukai adalah 
+ Bulutangkis";
+ }
+}
